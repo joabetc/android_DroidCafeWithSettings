1e137faaf9fd3ef7e061414d86311be95e80e3c9
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-01 16:00:23 -0300

    Adding a RadioGroup and radio buttons

--- app/src/main/java/com/example/android/droidcafe/OrderActivity.java
+++ app/src/main/java/com/example/android/droidcafe/OrderActivity.java
@@ -19,6 +19,7 @@ package com.example.android.droidcafe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
@@ -37,5 +38,8 @@ public class OrderActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+    }
+
+    public void onRadioButtonClicked(View view) {
     }
 }
