e4b5a11c3de94ad2c737635d2bf8fdcfb0359af4
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-02 13:35:36 -0300

    Starting an Activity from a menu item

--- app/src/main/java/com/example/android/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/android/droidcafe/MainActivity.java
@@ -89,7 +89,9 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
             case R.id.action_order:
-                displayToast(getString(R.string.action_order_message));
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
                 return true;
             case R.id.action_status:
                 displayToast(getString(R.string.action_status_message));
